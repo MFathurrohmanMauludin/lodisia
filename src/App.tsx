64c936465d3a5e9b63783dd34e26d14640173ecf
M Fathurrohman Mauludin <fathurdiakfari97@gmail.com>
2023-12-10 10:14:19 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="border border-cyan-400 px-4 py-2 text-cyan-500">Hello World</div>
+    </>
+  )
+}
+
+export default App
