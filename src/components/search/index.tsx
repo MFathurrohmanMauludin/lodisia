e0c5f2c9be535acf5a5751fd904a30a0293ad755
M Fathurrohman Mauludin <fathurdiakfari97@gmail.com>
2023-12-16 14:49:07 +0700

    update

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -1,11 +1,11 @@
-import { faSearch } from "@fortawesome/free-solid-svg-icons"
+import { faMicrophone, faSearch } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { Input } from "@nextui-org/react"
+import { Button, Input } from "@nextui-org/react"
 
 const SearchField = () => {
     return (
         <div className="flex justify-center pt-14 pb-4">
-            <div className="w-[500px]">
+            <div className="flex items-center w-[500px] gap-x-2">
                 <Input
                     variant="bordered"
                     color="default"
@@ -16,6 +16,16 @@ const SearchField = () => {
                     startContent={
                         <FontAwesomeIcon className="flex-shrink-0 text-gray-400" fontSize={16} icon={faSearch} />
                     }
+                />
+
+                <Button
+                    variant="light"
+                    color="default"
+                    size="md"
+                    radius="full"
+                    isIconOnly
+                    aria-label="aktifkan pencarian suara"
+                    startContent={<FontAwesomeIcon className="flex-shrink" fontSize={24} icon={faMicrophone} />}
                 />
             </div>
         </div>
