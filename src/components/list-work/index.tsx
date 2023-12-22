21ea801b69e4974ec50f8d6272567cd3269eeca3
M Fathurrohman Mauludin <fathurdiakfari97@gmail.com>
2023-12-22 23:58:12 +0700

    update

--- src/components/list-work/index.tsx
+++ src/components/list-work/index.tsx
@@ -5,6 +5,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye } from "@fortawesome/free-regular-svg-icons";
 import HeadLine from "../title";
 import { useLocation } from "react-router-dom";
+import { useState } from "react";
 
 interface Props {
     work: any;
@@ -12,6 +13,7 @@ interface Props {
 }
 
 const WorkList = (getData: Props) => {
+    // convert date
     const dayDate = (data: number) => new Date(data).getDate();
     const yearDate = (data: number) => new Date(data).getFullYear();
 
@@ -19,34 +21,70 @@ const WorkList = (getData: Props) => {
     const finishDate = (data: number) => new Date(data).toLocaleDateString('id', { day: 'numeric', month: 'long', year: 'numeric' });
     const countDay = (finish: number) => Math.ceil((new Date(finish).getTime() - new Date().getTime()) / ((1000 * 3600) * 24));
 
+    // get path url
     const location = useLocation();
     const { pathname } = location;
 
+    // limit show data
     const descData = getData.work.sort((a: any, b: any) => dayDate(b.startDate) - dayDate(a.startDate));
-
     const limitData = pathname === '/' ? descData.slice(0, 6) : descData;
+
+
+    // pagination
+    const itemsPerPage = 6;
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const totalPage = Math.ceil(getData.work.length / itemsPerPage);
+
+    const currentItems = getData.work.slice(indexOfFirstItem, indexOfLastItem);
+
+    const handlePageChange = (pageNumber: number) => {
+        setCurrentPage(pageNumber);
+    };
+
 
     return (
         <>
             <HeadLine headLine={getData.headLine} />
             <div className={`grid sm:grid-cols-1 lg:grid-cols-2 grid-cols-3 grid-flow-row gap-4`}>
                 {
-                    limitData.map((data: any) => (
-                        <CardJob
-                            key={data.id}
-                            id={data.id}
-                            logo={data.logo}
-                            name={data.name}
-                            company={data.company}
-                            level={data.levelWork}
-                            imgUrl={data.imgUrl}
-                            styleWork={data.styleWork}
-                            location={data.location}
-                            disability={data.disability}
-                            typeContract={data.typeContract}
-                            dateStarted={`${dayDate(data.startDate)} ${yearDate(data.startDate) !== yearDate(data.finishDate) ? startDate(data.startDate) : ''} - ${finishDate(data.finishDate)}`}
-                            countDown={`${countDay(data.finishDate) < 0 ? 0 : countDay(data.finishDate)}`} />
-                    ))
+                    pathname === '/' ?
+                        limitData.map((data: any) => (
+                            <CardJob
+                                key={data.id}
+                                id={data.id}
+                                logo={data.logo}
+                                name={data.name}
+                                company={data.company}
+                                level={data.levelWork}
+                                imgUrl={data.imgUrl}
+                                styleWork={data.styleWork}
+                                location={data.location}
+                                disability={data.disability}
+                                typeContract={data.typeContract}
+                                dateStarted={`${dayDate(data.startDate)} ${yearDate(data.startDate) !== yearDate(data.finishDate) ? startDate(data.startDate) : ''} - ${finishDate(data.finishDate)}`}
+                                countDown={`${countDay(data.finishDate) < 0 ? 0 : countDay(data.finishDate)}`} />
+                        ))
+                        :
+
+                        currentItems.map((data: any) => (
+                            <CardJob
+                                key={data.id}
+                                id={data.id}
+                                logo={data.logo}
+                                name={data.name}
+                                company={data.company}
+                                level={data.levelWork}
+                                imgUrl={data.imgUrl}
+                                styleWork={data.styleWork}
+                                location={data.location}
+                                disability={data.disability}
+                                typeContract={data.typeContract}
+                                dateStarted={`${dayDate(data.startDate)} ${yearDate(data.startDate) !== yearDate(data.finishDate) ? startDate(data.startDate) : ''} - ${finishDate(data.finishDate)}`}
+                                countDown={`${countDay(data.finishDate) < 0 ? 0 : countDay(data.finishDate)}`} />
+                        ))
                 }
             </div>
 
@@ -72,7 +110,13 @@ const WorkList = (getData: Props) => {
                     </div>
                     :
                     <div className="flex justify-center pt-8">
-                        <Pagination showControls total={10} initialPage={1} />
+                        <Pagination
+                            showControls
+                            total={totalPage}
+                            classNames={{ next: currentPage > 1 ? 'custom-next' : '', prev: currentPage < itemsPerPage ? 'custom-prev' : '' }}
+                            initialPage={currentPage}
+                            onChange={handlePageChange}
+                        />
                     </div>
             }
         </>
