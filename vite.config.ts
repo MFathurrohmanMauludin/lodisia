bfeaadef3daed28c431af10a6df00cb18799856c
M Fathurrohman Mauludin <fathurdiakfari97@gmail.com>
2023-12-24 23:33:40 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: '/google1f0e8001161825e8.html',
+          dest: 'dist'
+        }
+      ]
+    })
+  ],
 })
