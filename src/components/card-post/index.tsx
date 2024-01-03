7e5284b34ebe33c81d1b3ea166928234e1df3ac9
M Fathurrohman Mauludin <fathurdiakfari97@gmail.com>
2024-01-03 09:32:34 +0700

    update

---
+++ src/components/card-post/index.tsx
@@ -0,0 +1,13 @@
+import { Card, CardHeader } from "@nextui-org/react"
+
+const CardPosting = () => {
+    return (
+        <>
+            <Card>
+                <CardHeader />
+            </Card>
+        </>
+    )
+}
+
+export default CardPosting
